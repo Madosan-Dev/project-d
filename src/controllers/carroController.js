cfd2d248ce00fd48f7c0980b3de92de761fe61de
Madosan-Dev <matheus.sferreira@sptech.school>
2026-05-01 20:13:56 -0300

    feat: adicionando função cadastrarCarro no carroController.js

--- src/controllers/carroController.js
+++ src/controllers/carroController.js
@@ -4,8 +4,49 @@ function buscarCarro(){
 
 }
 
-function cadastrar(req,res){
+function cadastrarCarro(req,res){
+    // Estas variaveis pegam valores do validacoes.js, da função cadastrarCarro
+    let link = req.body.linkServer;
+    let marca = req.body.marcaServer;
+    let modelo = req.body.modeloServer;
+    let potencia = req.body.potenciaServer;
+    let peso = req.body.pesoServer;
+    let tracao = req.body.tracaoServer;
+    let fkUsuario = req.body.fkUsuarioServer;
 
-    
+    if(link == undefined){
+        res.status(400).send("O link está undefined!");
+    } else if(marca == undefined){
+        res.status(400).send("A marca está undefined!");
+    } else if(modelo == undefined){
+        res.status(400).send("O modelo está undefined");
+    } else if(potencia == undefined){
+        res.status(400).send("A potencia está undefined");
+    } else if(peso == undefined){
+        res.status(400).send("O peso está undefined");
+    } else if(tracao == undefined){
+        res.status(400).send("A tração está undefined");
+    } else if(fkUsuario == undefined){
+        res.status(400).send("O fkUsuario está undefined");
+    }
 
+        carroModel.cadastrarCarro(link,marca,modelo,potencia,peso,tracao)
+             .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro do carro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+}
+
+module.exports = {
+    cadastrarCarro
 }
