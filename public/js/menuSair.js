a903883a67d3395308f10d7ec0fe6307598d1f7e
Madosan-Dev <matheus.sferreira@sptech.school>
2026-05-02 17:59:59 -0300

    feat: adiciona menu de sair

---
+++ public/js/menuSair.js
@@ -0,0 +1,7 @@
+function abrirMenu(){
+    document.getElementById('tela_escura').style.display = 'flex';
+}
+function fecharMenu(){
+    document.getElementById('tela_escura').style.display = 'none';
+}
+
