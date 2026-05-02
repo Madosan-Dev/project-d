483a452a376a2d53d9bc77e2691e9f87fad638cc
Madosan-Dev <matheus.sferreira@sptech.school>
2026-05-01 21:12:44 -0300

    feat: adiciona module.exports no carros.js

--- src/routes/carros.js
+++ src/routes/carros.js
@@ -1,9 +1,11 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
-let carroController = require('../controllers/carroController');
+let carroController = require("../controllers/carroController");
 
 // recebendo dados do HTML e direcionando para a função cadastrarCarro do carroController
 router.post("/cadastrar", function (req, res){
-    carroController.cadastrarCarro(req,res);
+    carroController.cadastrarCarro(req, res);
 });
+
+module.exports = router;
