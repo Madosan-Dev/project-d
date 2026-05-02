e4c248769a6ce1fda98a5598b2fef45d3e4c0a12
Madosan-Dev <matheus.sferreira@sptech.school>
2026-05-01 21:42:32 -0300

    feat: adiciona verificação se está logado ao index

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -12,8 +12,36 @@ function validarSessao() {
     }
 }
 
+function verificarLogado(){
+    const btnLog = document.getElementById('btn_login');
+    const btnCad = document.getElementById('btn_cad');
+
+    let id = sessionStorage.ID_USUARIO;
+    let email = sessionStorage.EMAIL_USUARIO;
+    let nome = sessionStorage.NOME_USUARIO;
+
+    if(email != null && id != null && nome != null){
+        btnCad.display = 'none';
+        btnLog.display = 'none';
+
+        btns.innerHTML = `<a href="dashboard/dashboard.html">Dashboard</a>`;
+        btns.innerHTML += `<a href="dashboard/perfil.html">Perfil</a>`;
+        btns.innerHTML += `<a onclick="limparSessao()">Sair</a>`;
+    }
+
+}
+
 function inserirFoto(){
     var link = sessionStorage.LINK_USUARIO;
+    var foto = document.getElementById("div_foto");
+
+    if(link != null){
+        foto.innerHTML = `<img src="${link}" class="foto_perfil">`;
+    }
+}
+
+function inserirFotoCarro(){
+    var link = sessionStorage.LINK_CARRO;
     var foto = document.getElementById("div_foto");
 
     if(link != null){
