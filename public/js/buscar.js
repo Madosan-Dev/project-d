35a698b99e0257dd38acf08df131f60f4148673e
Madosan-Dev <matheus.sferreira@sptech.school>
2026-05-02 01:25:03 -0300

    fix: corrige a função para receber o link e exibir

---
+++ public/js/buscar.js
@@ -0,0 +1,24 @@
+function buscarCarro(){
+    let idUsuario = sessionStorage.ID_USUARIO;
+
+    fetch(`/carros/buscar/${idUsuario}`)
+            .then(function (resposta) {
+                console.log("resposta: ", resposta);
+
+                if (resposta.ok) {
+                    resposta.json().then(function (dados){
+
+                        let link = dados[0].url_foto;
+
+                        div_foto.innerHTML = `<img src="${link}" class="foto_carro"  onerror="this.onerror=null; this.src='assets/icones/foto_invalida.png';">`;
+                    });
+                } else {
+                throw "Houve um erro ao buscar os dados!";
+                }
+            })
+            .catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+                finalizarAguardar();
+        });
+
+}
