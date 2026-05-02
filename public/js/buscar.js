ac4e897f34e30b17c69f67ec56a1d51adb189bbd
Madosan-Dev <matheus.sferreira@sptech.school>
2026-05-02 02:16:20 -0300

    feat: adiciona uma mensagem para redirecionar para o cadastro do carro, caso o usuario não tenha

--- public/js/buscar.js
+++ public/js/buscar.js
@@ -8,9 +8,16 @@ function buscarCarro(){
                 if (resposta.ok) {
                     resposta.json().then(function (dados){
 
-                        let link = dados[0].url_foto;
+                        if(dados.length > 0){
+                            let link = dados[0].url_foto;
+    
+                            div_foto.innerHTML = `<img src="${link}" class="foto_carro"  onerror="this.onerror=null; this.src='assets/icones/foto_invalida.png';">`;
+                        }else{
+                            div_foto.innerHTML = `<h1>Você ainda não tem carro cadastrado</h1>
+                            <br>
+                                <a href="../cadastro-carro.html">Cadastre aqui!</a> `;
+                        }
 
-                        div_foto.innerHTML = `<img src="${link}" class="foto_carro"  onerror="this.onerror=null; this.src='assets/icones/foto_invalida.png';">`;
                     });
                 } else {
                 throw "Houve um erro ao buscar os dados!";
